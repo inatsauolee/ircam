a9b5a710c6f495cad80b62d6e7294c381fcdef01
ahmed el hajouti <ahmedtazimi@gmail.com>
2020-02-03 22:46:31 +0000

    db2

--- database/ircam.sql
+++ database/ircam.sql
@@ -7,8 +7,10 @@
 -- Version du serveur: 5.6.12-log
 -- Version de PHP: 5.4.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,7 +21,10 @@ SET time_zone = "+00:00";
 --
 -- Base de données: `ircam`
 --
-CREATE DATABASE IF NOT EXISTS `ircam` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE
+IF NOT EXISTS `ircam` DEFAULT CHARACTER
+SET utf8
+COLLATE utf8_general_ci;
 USE `ircam`;
 
 -- --------------------------------------------------------
@@ -28,19 +33,29 @@ USE `ircam`;
 -- Structure de la table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `label` varchar(100) COLLATE utf8_bin NOT NULL,
-  `creator` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `creator` (`creator`)
+CREATE TABLE
+IF NOT EXISTS `category`
+(
+  `ID` int
+(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar
+(100) COLLATE utf8_bin NOT NULL,
+  `creator` int
+(11) NOT NULL,
+  PRIMARY KEY
+(`ID`),
+  KEY `creator`
+(`creator`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=48 ;
 
 --
 -- Contenu de la table `category`
 --
 
-INSERT INTO `category` (`ID`, `label`, `creator`) VALUES
+INSERT INTO `category` (`
+ID`,
+`label
+`, `creator`) VALUES
 (1, 'communication', 1),
 (30, 'Animal', 1),
 (31, 'Fruit', 1),
@@ -67,19 +82,26 @@ INSERT INTO `category` (`ID`, `label`, `creator`) VALUES
 -- Structure de la table `expression`
 --
 
-CREATE TABLE IF NOT EXISTS `expression` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE
+IF NOT EXISTS `expression`
+(
+  `ID` int
+(10) NOT NULL AUTO_INCREMENT,
   `label` text NOT NULL,
   `word_group` text NOT NULL,
   `translation` text NOT NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY
+(`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `expression`
 --
 
-INSERT INTO `expression` (`ID`, `label`, `word_group`, `translation`) VALUES
+INSERT INTO `expression` (`
+ID`,
+`label
+`, `word_group`, `translation`) VALUES
 (1, 'masin issawal Tamazivt', '1,2', 'masin talks in Tamazight');
 
 -- --------------------------------------------------------
@@ -88,21 +110,33 @@ INSERT INTO `expression` (`ID`, `label`, `word_group`, `translation`) VALUES
 -- Structure de la table `grammar`
 --
 
-CREATE TABLE IF NOT EXISTS `grammar` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `tawalt_ID` int(10) NOT NULL,
-  `past` varchar(100) NOT NULL,
-  `present` varchar(100) NOT NULL,
-  `future` varchar(100) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `tawalt_ID` (`tawalt_ID`)
+CREATE TABLE
+IF NOT EXISTS `grammar`
+(
+  `ID` int
+(10) NOT NULL AUTO_INCREMENT,
+  `tawalt_ID` int
+(10) NOT NULL,
+  `past` varchar
+(100) NOT NULL,
+  `present` varchar
+(100) NOT NULL,
+  `future` varchar
+(100) NOT NULL,
+  PRIMARY KEY
+(`ID`),
+  KEY `tawalt_ID`
+(`tawalt_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `grammar`
 --
 
-INSERT INTO `grammar` (`ID`, `tawalt_ID`, `past`, `present`, `future`) VALUES
+INSERT INTO `grammar` (`
+ID`,
+`tawalt_ID
+`, `past`, `present`, `future`) VALUES
 (1, 1, 'ssiwlev, tessiwled, issiwel, tessiwel, nessiwel, tessiwlem, tessiwlent, ssiwlen, ssiwlent', 'ssawalev, tessawaled, issawal, tessawal, nessawal, tessawalem, tessawalent, ssawalen, ssawalent', 'ad ssiwlev, ad tessiwled, ad issiwel, ad tessiwel, ad nessiwel, ad tessiwlem, ad tessiwlent, ad ssiw');
 
 -- --------------------------------------------------------
@@ -111,25 +145,41 @@ INSERT INTO `grammar` (`ID`, `tawalt_ID`, `past`, `present`, `future`) VALUES
 -- Structure de la table `tawalt`
 --
 
-CREATE TABLE IF NOT EXISTS `tawalt` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `tifinagh` varchar(100) NOT NULL,
-  `label` varchar(100) NOT NULL,
-  `type` int(11) NOT NULL DEFAULT '0',
-  `category` int(11) NOT NULL,
-  `creator` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `type` (`type`),
-  KEY `category` (`category`),
-  KEY `creator` (`creator`),
-  KEY `creator_2` (`creator`)
+CREATE TABLE
+IF NOT EXISTS `tawalt`
+(
+  `ID` int
+(10) NOT NULL AUTO_INCREMENT,
+  `tifinagh` varchar
+(100) NOT NULL,
+  `label` varchar
+(100) NOT NULL,
+  `type` int
+(11) NOT NULL DEFAULT '0',
+  `category` int
+(11) NOT NULL,
+  `creator` int
+(11) NOT NULL,
+  PRIMARY KEY
+(`ID`),
+  KEY `type`
+(`type`),
+  KEY `category`
+(`category`),
+  KEY `creator`
+(`creator`),
+  KEY `creator_2`
+(`creator`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `tawalt`
 --
 
-INSERT INTO `tawalt` (`ID`, `tifinagh`, `label`, `type`, `category`, `creator`) VALUES
+INSERT INTO `tawalt` (`
+ID`,
+`tifinagh
+`, `label`, `type`, `category`, `creator`) VALUES
 (1, 'issiwel', 'issiwel', 1, 1, 1),
 (2, 'tamazivt', 'tamaziɣt', 0, 1, 1);
 
@@ -139,19 +189,29 @@ INSERT INTO `tawalt` (`ID`, `tifinagh`, `label`, `type`, `category`, `creator`) 
 -- Structure de la table `type`
 --
 
-CREATE TABLE IF NOT EXISTS `type` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `label` varchar(100) COLLATE utf8_bin NOT NULL,
-  `creator` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `creator` (`creator`)
+CREATE TABLE
+IF NOT EXISTS `type`
+(
+  `ID` int
+(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar
+(100) COLLATE utf8_bin NOT NULL,
+  `creator` int
+(11) NOT NULL,
+  PRIMARY KEY
+(`ID`),
+  KEY `creator`
+(`creator`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `type`
 --
 
-INSERT INTO `type` (`ID`, `label`, `creator`) VALUES
+INSERT INTO `type` (`
+ID`,
+`label
+`, `creator`) VALUES
 (0, 'noun', 1),
 (1, 'verb', 1);
 
@@ -161,23 +221,35 @@ INSERT INTO `type` (`ID`, `label`, `creator`) VALUES
 -- Structure de la table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` int(2) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `ID` (`ID`)
+CREATE TABLE
+IF NOT EXISTS `user` (
+  `ID` int
+(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar
+(100) NOT NULL,
+  `last_name` varchar
+(100) NOT NULL,
+  `email` varchar
+(100) NOT NULL,
+  `username` varchar
+(50) NOT NULL,
+  `password` varchar
+(100) NOT NULL,
+  `role` int
+(2) NOT NULL,
+  PRIMARY KEY
+(`ID`),
+  KEY `ID`
+(`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`ID`, `first_name`, `last_name`, `email`, `username`, `password`, `role`) VALUES
+INSERT INTO `user`
+  (`ID`, `first_name
+`, `last_name`, `email`, `username`, `password`, `role`) VALUES
 (1, 'Ahmed', 'ELHAJOUTI', '', 'admin', '123', 0),
 (2, 'Mohamed', 'ELOUASTANI', '', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 0);
 
@@ -187,19 +259,29 @@ INSERT INTO `user` (`ID`, `first_name`, `last_name`, `email`, `username`, `passw
 -- Structure de la table `word`
 --
 
-CREATE TABLE IF NOT EXISTS `word` (
-  `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `tawalt_ID` int(10) NOT NULL,
-  `label` varchar(100) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `tawalt_ID` (`tawalt_ID`)
+CREATE TABLE
+IF NOT EXISTS `word`
+(
+  `ID` int
+(10) NOT NULL AUTO_INCREMENT,
+  `tawalt_ID` int
+(10) NOT NULL,
+  `label` varchar
+(100) NOT NULL,
+  PRIMARY KEY
+(`ID`),
+  KEY `tawalt_ID`
+(`tawalt_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `word`
 --
 
-INSERT INTO `word` (`ID`, `tawalt_ID`, `label`) VALUES
+INSERT INTO `word` (`
+ID`,
+`tawalt_ID
+`, `label`) VALUES
 (1, 1, 'talk'),
 (2, 2, 'tamazight');
 
@@ -211,33 +293,55 @@ INSERT INTO `word` (`ID`, `tawalt_ID`, `label`) VALUES
 -- Contraintes pour la table `category`
 --
 ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_2` FOREIGN KEY (`creator`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `category_ibfk_2` FOREIGN KEY
+(`creator`) REFERENCES `user`
+(`ID`) ON
+DELETE CASCADE ON
+UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `grammar`
 --
 ALTER TABLE `grammar`
-  ADD CONSTRAINT `grammar_tawalt_ID` FOREIGN KEY (`tawalt_ID`) REFERENCES `tawalt` (`ID`);
+ADD CONSTRAINT `grammar_tawalt_ID` FOREIGN KEY
+(`tawalt_ID`) REFERENCES `tawalt`
+(`ID`);
 
 --
 -- Contraintes pour la table `tawalt`
 --
 ALTER TABLE `tawalt`
-  ADD CONSTRAINT `tawalt_ibfk_2` FOREIGN KEY (`category`) REFERENCES `category` (`ID`),
-  ADD CONSTRAINT `tawalt_ibfk_1` FOREIGN KEY (`type`) REFERENCES `type` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tawalt_ibfk_3` FOREIGN KEY (`creator`) REFERENCES `tawalt` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `tawalt_ibfk_2` FOREIGN KEY
+(`category`) REFERENCES `category`
+(`ID`),
+ADD CONSTRAINT `tawalt_ibfk_1` FOREIGN KEY
+(`type`) REFERENCES `type`
+(`ID`) ON
+DELETE CASCADE ON
+UPDATE CASCADE,
+ADD CONSTRAINT `tawalt_ibfk_3` FOREIGN KEY
+(`creator`) REFERENCES `tawalt`
+(`ID`) ON
+DELETE CASCADE ON
+UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `type`
 --
 ALTER TABLE `type`
-  ADD CONSTRAINT `type_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `type_ibfk_1` FOREIGN KEY
+(`creator`) REFERENCES `user`
+(`ID`) ON
+DELETE CASCADE ON
+UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `word`
 --
 ALTER TABLE `word`
-  ADD CONSTRAINT `word_tawalt_ID` FOREIGN KEY (`tawalt_ID`) REFERENCES `tawalt` (`ID`);
+ADD CONSTRAINT `word_tawalt_ID` FOREIGN KEY
+(`tawalt_ID`) REFERENCES `tawalt`
+(`ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
